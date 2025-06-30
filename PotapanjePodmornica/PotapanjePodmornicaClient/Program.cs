51a9661851fff5c7fee8c73d556670f740cdcac4
skopi1201 <kaporjoki1@gmail.com>
2025-06-30 21:03:15 +0200

    test server/client communication need another client

---
+++ PotapanjePodmornica/PotapanjePodmornicaClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PotapanjePodmornicaClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverIP = "127.0.0.1";
+            int serverUdpPort = 9000;
+
+            
+            using (UdpClient udpClient = new UdpClient())
+            {
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIP), serverUdpPort);
+
+                while (true)
+                {
+                    // Klijent šalje poruku prvi
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("You say: ");
+                    string messageToSend = Console.ReadLine();
+                    byte[] dataToSend = Encoding.UTF8.GetBytes(messageToSend);
+                    udpClient.Send(dataToSend, dataToSend.Length, serverEP);
+                    Console.ResetColor();
+
+                    // Čekaj odgovor od servera
+                    IPEndPoint fromEP = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receivedData = udpClient.Receive(ref fromEP);
+                    string receivedMessage = Encoding.UTF8.GetString(receivedData);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Server says: {receivedMessage}");
+                    Console.ResetColor() ;
+                }
+            }
+        }
+    }
+}
---
+++ PotapanjePodmornica/PotapanjePodmornicaServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PotapanjePodmornicaServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int udpPort = 9000;
+           
+
+            using (UdpClient udpServer = new UdpClient(udpPort))
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+                
+
+                string receivedMessage = null;
+
+                while (true)
+                {
+                    // Čekaj poruku od klijenta
+                    byte[] receivedData = udpServer.Receive(ref remoteEP);
+                    receivedMessage = Encoding.UTF8.GetString(receivedData);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Client says: {receivedMessage}");
+                    Console.ResetColor();
+
+                    // Sada je tvoj red za slanje
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("You say: ");
+                    string messageToSend = Console.ReadLine();
+                    byte[] sendData = Encoding.UTF8.GetBytes(messageToSend);
+                    udpServer.Send(sendData, sendData.Length, remoteEP);
+                    Console.ResetColor();
+
+                }
+            }
+        }
+    }
+}
